95da3b0c580f1f25b8af8b421cecedf7d5c9ca47
Lenko Ivanov <72096565+LenkoIvanov@users.noreply.github.com>
2021-09-22 20:57:53 +0300

    button event added, to fix solver method

--- EntryPoint.java
+++ EntryPoint.java
@@ -1,23 +1,6 @@
 import java.lang.*;
 
 public class EntryPoint {
-
-    public static void quadraticSolver(double a,double b, double c, double x1, double x2, double x){
-        double det = 0.0;
-        det = (b*b) - (4*a*c);
-        if(det > 0.0){
-            x1 = (-b + Math.sqrt(det))/(2*a); //two solutions
-            x2 = (-b - Math.sqrt(det))/(2*a);
-        }
-        else if(det == 0.0){
-            x = -b/(2*a); //one solution
-        }
-        else{
-            //no real solutions
-            //add error message
-        }
-    }
-
     public static void main(String[] args){
         GUIFrame.setLookAndFeel();
         GUIFrame gui = new GUIFrame();
--- GUIFrame.java
+++ GUIFrame.java
@@ -1,14 +1,15 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
-public class GUIFrame extends JFrame{
+public class GUIFrame extends JFrame implements ActionListener{
     //entry row for a,b,c
     JPanel row1 = new JPanel();
     JLabel numA = new JLabel("a", JLabel.RIGHT);
     JTextField aEntry = new JTextField("0");
     JLabel numB = new JLabel("b", JLabel.RIGHT);
     JTextField bEntry = new JTextField("0");
-    JLabel numC = new JLabel("c",JLabel.RIGHT);
+    JLabel numC = new JLabel("c", JLabel.RIGHT);
     JTextField cEntry = new JTextField("0");
 
     //row for buttons
@@ -33,27 +34,31 @@ public class GUIFrame extends JFrame{
         setLayout(layout);
 
         //layout for row1
-        GridLayout layout1 = new GridLayout(1, 4,10,10);
+        GridLayout layout1 = new GridLayout(1, 4, 10, 10);
         row1.setLayout(layout1);
         row1.add(numA);
         row1.add(aEntry);
+        aEntry.addActionListener(this);
         row1.add(numB);
         row1.add(bEntry);
+        bEntry.addActionListener(this);
         row1.add(numC);
         row1.add(cEntry);
-        add(row1,BorderLayout.NORTH);
+        cEntry.addActionListener(this);
+        add(row1, BorderLayout.NORTH);
 
         //layout for row2
-        FlowLayout layout2 = new FlowLayout(FlowLayout.CENTER,10,10);
+        FlowLayout layout2 = new FlowLayout(FlowLayout.CENTER, 10, 10);
         row2.setLayout(layout2);
-        calc.setEnabled(false);
+        calc.setEnabled(true);
         draw.setEnabled(false);
         row2.add(calc);
+        calc.addActionListener(this);
         row2.add(draw);
-        add(row2,BorderLayout.CENTER);
+        add(row2, BorderLayout.CENTER);
 
         //layout for row3
-        GridLayout layout3 = new GridLayout(1,4,10,10);
+        GridLayout layout3 = new GridLayout(1, 4, 10, 10);
         row3.setLayout(layout3);
         row3.add(numX1);
         row3.add(outX1);
@@ -61,19 +66,43 @@ public class GUIFrame extends JFrame{
         row3.add(outX2);
         row3.add(numX);
         row3.add(outX);
-        add(row3,BorderLayout.SOUTH);
+        add(row3, BorderLayout.SOUTH);
 
         setVisible(true);
         pack();
         setResizable(false);
     }
 
-    public static void setLookAndFeel(){
-        try{
-            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-        }catch(Exception exc){
+    public void actionPerformed(ActionEvent e){
+        String s1 = aEntry.getText();
+        String s2 = bEntry.getText();
+        String s3 = cEntry.getText();
+        //int a = Integer.parseInt(s1);
+       // int b = Integer.parseInt(s2);
+        //int c = Integer.parseInt(s3);
+       // int x1 = 0;
+       // int x2 = 0;
+       // int x = 0;
+        if(e.getSource() == calc){
+           // EntryPoint.quadraticSolver(a,b,c,x1,x2,x);
+            //String s4 = String.valueOf(x1);
+           // String s5 = String.valueOf(x2);
+            //String s6 = String.valueOf(x);
+            outX1.setText(s1);
+            outX2.setText(s2);
+            outX.setText(s3);
+        }
+        else{
             //
+
         }
     }
 
+    public static void setLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception exc) {
+            //
+        }
+    }
 }
